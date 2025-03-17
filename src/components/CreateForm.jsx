7c59314eca2a756ed325b6542bf427df7e6501df
cbs55137 <bbbcarry3232@gmail.com>
2025-03-17 10:47:47 +0800

    做新增todo的功能

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateForm = () => {
+  const [content, setContent] = useState("");
+
+  const btnHandler = (e) => {
+    e.preventDefault();
+    setContent("");
+  };
+
+  const inputHandler = (e) => {
+    content = e.target.value;
+  };
+
   return (
     <form className="CreateForm">
-      <input type="text" placeholder="<<< 輸入 <<<" id="inputText" />
-      <button id="addBtn" type="submit">
+      <input
+        type="text"
+        placeholder="<<< 輸入 <<<"
+        id="inputText"
+        value={content}
+        onChange={inputHandler}
+      />
+      <button id="addBtn" type="submit" onClick={btnHandler}>
         加入
       </button>
     </form>
