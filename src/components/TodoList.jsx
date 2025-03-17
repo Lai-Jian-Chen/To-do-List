0b25a1e4e7c4eb1b3c551260d7d883663d14f004
cbs55137 <bbbcarry3232@gmail.com>
2025-03-17 12:41:16 +0800

    做完成事項的視覺反饋

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { CiEdit } from "react-icons/ci";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
-const TodoList = ({ todo }) => {
+const TodoList = ({ todo, deleteTodo }) => {
+  const deleteHandler = () => deleteTodo(todo.id);
+
   return (
     <div className="TodoList">
       <p>{todo.content}</p>
       <div>
         <CiEdit className="list-Btn" />
-        <RiDeleteBin6Line className="list-Btn" />
+        <RiDeleteBin6Line className="list-Btn" onClick={deleteHandler} />
       </div>
     </div>
   );
--- src/components/todoWrapper.jsx
+++ src/components/todoWrapper.jsx
@@ -12,15 +12,22 @@ const todoWrapper = () => {
     setTodos([...todos, { content, id: Math.random() }]);
   };
 
+  const deleteTodo = (id) => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== id;
+      })
+    );
+  };
+
   return (
     <div className="todoWrapper">
       <h1>待辦事項</h1>
       <CreateForm addTodo={addTodo} />
       {todos.map((todo) => {
-        return <TodoList todo={todo} key={todo.id} />;
+        return <TodoList todo={todo} key={todo.id} deleteTodo={deleteTodo} />;
       })}
     </div>
   );
 };
-
 export default todoWrapper;
